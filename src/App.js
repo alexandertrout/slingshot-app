ee2ea814e1edcfa5b01cbc996b58b60ce9041a7d
Alexander Trout <alexandertrout@A1.local>
2020-03-26 13:41:13 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,88 @@
 import React from 'react';
-import logo from './logo.svg';
+// import ReactDOM from 'react-dom';
+import { makeStyles } from '@material-ui/core/styles';
+import Amplify from 'aws-amplify';
+import awsconfig from './aws-exports';
+import { withAuthenticator } from 'aws-amplify-react';
 import './App.css';
+import Graph from './Components/Graph';
+import { Container, Button, FormGroup, Switch, FormControlLabel, Box, Paper, Grid } from '@material-ui/core';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    background: 'linear-gradient(45deg, #A4C6D3 30%, #D3D4D5 90%)',
+    border: 0,
+    borderRadius: 3,
+    color: 'white',
+    margin: 0,
+    padding: 20,
+  },
+  button: {
+    background: 'black',
+    border: 0,
+    borderRadius: 3,
+    boxShadow: '0 3px 5px 2px grey',
+    color: 'white',
+    padding: '0 30px',
+  },
+  // rootGrid: {
+  //   flexGrow: 1,
+  // },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+    margin: 0,
+    height: 250,
+  },
+}));
+
+Amplify.configure(awsconfig);
 
 function App() {
+  //state management
+  const [checked, setChecked] = React.useState(true);
+
+  //functions
+  const toggleChecked = () => {
+    setChecked(!checked);
+  };
+
+  //rendering
+  const classes = useStyles();
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <Container className={classes.root}> */}
+        <Box className={classes.root}>
+
+        <h1>Welcome To Your Dashboard</h1>
+
+        <Grid container spacing={3}>
+          <Grid item xs={12}>
+            <Paper className={classes.paper}>
+               Insert a Graph Here
+               <Graph/>
+            </Paper>
+          </Grid>
+          <Grid item xs={6}>
+            <Paper className={classes.paper}> 
+              Insert Equation Editing Here
+              <Switch checked={checked} onClick={toggleChecked} /> 
+              <Button className={classes.button}>click me!</Button> 
+            </Paper>
+          </Grid>
+          <Grid item xs={6}>
+            <Paper className={classes.paper}>
+              Insert Machine Learning Here
+              <Switch checked={checked} onClick={toggleChecked} />
+              <Button className={classes.button}>click me!</Button> 
+            </Paper>
+          </Grid>
+        </Grid>
+        </Box>
+      {/* </Container> */}
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App, true);
---
+++ src/Components/Graph.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import { Container, Button, FormGroup, Switch, FormControlLabel, Box, Paper, Grid } from '@material-ui/core';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    background: 'lightgrey',
+    border: 0,
+    borderRadius: 3,
+    color: 'white',
+    margin: 5,
+    padding: 20,
+  },
+  button: {
+    background: 'black',
+    border: 0,
+    borderRadius: 3,
+    boxShadow: '0 3px 5px 2px grey',
+    color: 'white',
+    padding: '0 30px',
+  },
+}));
+
+const Graph = (props) => {
+  //state management
+  const [checked, setChecked] = React.useState(true);
+
+  //functions
+  const toggleChecked = () => {
+    setChecked(!checked);
+  };
+
+  //rendering
+  const classes = useStyles();
+  return (
+      <Box className={classes.root}>
+      <Button className={classes.button}>click me!</Button>
+      <Switch checked={checked} onClick={toggleChecked} />
+      </Box>
+  );
+};
+
+export default Graph;
